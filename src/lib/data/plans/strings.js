f7ab0c708dcdc7ce1bf553b8fd839f745795eb2d
tour-hotz <>
2025-07-22 13:52:27 +0530

    added strings

---
+++ src/lib/data/plans/strings.js
@@ -0,0 +1,57 @@
+export default {
+	title: 'Strings',
+	sections: [
+		{
+			name: 'Strings I',
+			problems: [
+				{
+					name: 'Number of Provinces',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/number-of-provinces/ ',
+					githubUrl:
+						'https://github.com/MAZHARMIK/Interview_DS_Algo/blob/master/Graph/BFS_DFS/Number%20of%20Provinces.cpp',
+					youtubeUrl:
+						'https://www.youtube.com/watch?v=70LNE8RMPNc&list=PLry9FeA3DJcGiIxITQGVJQgtLF8-XB5J0&index=1&t=116s',
+					codeUrl: '/code/number-of-provinces'
+				}
+			]
+		},
+		{
+			name: 'Strings II',
+			problems: [
+				{
+					name: 'Find the Index of the First Occurrence in a String',
+					completed: false,
+					difficulty: 'easy',
+					platform: 'leetcode',
+					url: ' https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/',
+					githubUrl: '# ',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'KMP Algorithm',
+					completed: false,
+					difficulty: 'hard',
+					platform: 'gfg',
+					url: 'https://www.geeksforgeeks.org/problems/search-pattern0205/1',
+					githubUrl: '# ',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Minimum Insertion Steps to Make a String Palindrome',
+					completed: false,
+					difficulty: 'hard',
+					platform: 'leetcode',
+					url: ' https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/',
+					githubUrl: '# ',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		}
+	]
+};
