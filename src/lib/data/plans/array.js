9a513eead3e22c28d88003c56f9b6bba347a07f6
PrashansaSoni <prashansasoni534@gmail.com>
2025-07-24 01:11:55 +0530

    add array

---
+++ src/lib/data/plans/array.js
@@ -0,0 +1,136 @@
+export default {
+	title: 'Array ',
+	sections: [
+		{
+			name: 'Array I',
+			problems: [
+				{
+					name: 'Set Matrix Zeroes',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/set-matrix-zeroes/description/ ',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Pascal\'s Triangle',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/pascals-triangle/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Next Permutation',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/next-permutation/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Maximum Subarray',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/maximum-subarray/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Sort Colors',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/sort-colors/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Best Time to Buy and Sell Stock',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		},
+		{
+			name: 'Array II',
+			problems: [
+				{
+					name: 'Rotate Image',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/rotate-image/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Merge Intervals',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'gfg',
+					url: 'https://leetcode.com/problems/merge-intervals/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Merge Sorted Array',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/merge-sorted-array/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Find the Duplicate Number',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/find-the-duplicate-number/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Search a 2D Matrix',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/search-a-2d-matrix/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Pow(x, n)',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/powx-n/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		}
+        
+	]
+};
