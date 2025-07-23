9437d3e0c47de27ffac5aea48b92fbe7f5c440ef
PrashansaSoni <prashansasoni534@gmail.com>
2025-07-23 22:14:12 +0530

    stack and queue

---
+++ src/lib/data/plans/queue.js
@@ -0,0 +1,88 @@
+export default {
+	title: 'Queue ',
+	sections: [
+		{
+			name: 'Queue I',
+			problems: [
+				{
+					name: 'Queue Using Array',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'gfg',
+					url: 'https://www.geeksforgeeks.org/problems/implement-queue-using-array/1',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: '  Implement Queue using Stacks',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/implement-queue-using-stacks/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Rotting Oranges',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/rotting-oranges/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Orderly Queue',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/orderly-queue/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				
+				
+			]
+		},
+		{
+			name: 'Queue II',
+			problems: [
+				{
+					name: 'Design Circular Queue',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/design-circular-queue/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Queue Reconstruction by Height',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/queue-reconstruction-by-height/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Number of Visible People in a Queue',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/number-of-visible-people-in-a-queue/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				
+			]
+		}
+	]
+};
---
+++ src/lib/data/plans/stack.js
@@ -0,0 +1,126 @@
+export default {
+	title: 'Stack ',
+	sections: [
+		{
+			name: 'Stack I',
+			problems: [
+				{
+					name: 'Implement stack using array',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'gfg',
+					url: 'https://www.geeksforgeeks.org/problems/implement-stack-using-array/1 ',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Implement Stack using Queues',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/implement-stack-using-queues/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Valid Parentheses',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/valid-parentheses/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Next Greater Element I',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/next-greater-element-i/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Sort a stack',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'gfg',
+					url: 'https://www.geeksforgeeks.org/problems/sort-a-stack/1',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				
+			]
+		},
+		{
+			name: 'Stack II',
+			problems: [
+				{
+					name: 'Largest Rectangle in Histogram',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/largest-rectangle-in-histogram/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Sliding Window Maximum',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/sliding-window-maximum/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Min Stack',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/min-stack/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Online Stock Span',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/online-stock-span/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Maximum Frequency Stack',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/maximum-frequency-stack/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Maximum Height by Stacking Cuboids',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/maximum-height-by-stacking-cuboids/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		}
+	]
+};
