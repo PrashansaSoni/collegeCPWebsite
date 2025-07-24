d94c1ce8051cc26289e45468244470124e137473
PrashansaSoni <prashansasoni534@gmail.com>
2025-07-24 14:02:22 +0530

    add linked list

---
+++ src/lib/data/plans/linkedlist.js
@@ -0,0 +1,136 @@
+export default {
+	title: 'LinkedList ',
+	sections: [
+		{
+			name: 'LinkedList I',
+			problems: [
+				{
+					name: ' Reverse Linked List',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/reverse-linked-list/description/ ',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Middle of the Linked List',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/middle-of-the-linked-list/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Merge Two Sorted Lists',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/merge-two-sorted-lists/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Remove Nth Node From End of List',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Add Two Numbers',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/add-two-numbers/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Delete Node in a Linked List',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/delete-node-in-a-linked-list/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		},
+		{
+			name: 'LinkedList II',
+			problems: [
+				{
+					name: 'Intersection of Two Linked Lists',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/intersection-of-two-linked-lists/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Linked List Cycle',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/linked-list-cycle/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Reverse Nodes in k-Group',
+					completed: false,
+					difficulty: 'Hard',
+					platform: 'leetcode',
+					url: ' Reverse Nodes in k-Group',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: 'Palindrome Linked List',
+					completed: false,
+					difficulty: 'Easy',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/palindrome-linked-list/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Linked List Cycle II',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/linked-list-cycle-ii/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				},
+				{
+					name: ' Rotate List',
+					completed: false,
+					difficulty: 'Medium',
+					platform: 'leetcode',
+					url: 'https://leetcode.com/problems/rotate-list/description/',
+					githubUrl: '#',
+					youtubeUrl: '#',
+					codeUrl: '#'
+				}
+			]
+		}
+        
+	]
+};
